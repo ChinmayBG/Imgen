62f43faea3edbf9dfca4fd60a11c7a95e22324d4
ChinmayBG <chinmayghodake7@.com>
2026-01-10 09:33:50 +0530

    created userLogin controller function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -33,3 +33,31 @@ const userRegistration = async (req,res) => {
     res.json({success:false,message:error.message})
   }
 }
+
+const loginUser = async () => {
+  
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+
+    // if user not exits login is not possible
+    if (!user) {
+      return res({success:false , message:"User doesnt exist"})
+    }
+
+    //if user exists then we check for password 
+    const isMatch = await bycrypt.compare(password, user.password)
+    
+    if (isMatch) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+      
+      res.json({success:true,token,user:{name:user.name}})
+    } else {
+      return res({success:false,message:"Invalid credentials"})
+    }
+    
+  } catch (error) {
+    console.log(error)
+    res.json({success:false , message:error.message})
+  }  
+} 
